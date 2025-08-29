d9a2e3b16c921062dde970a4dc033d027d9144d6
Akash-Brar <akashbrar467@gmail.com>
2025-08-28 18:31:39 -0600

    Added Contact Section with no alerts

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import About from "./sections/About";
 import Projects from "./sections/Projects";
 import Experiences from "./sections/Experiences";
 import Testimonial from "./sections/Testimonial";
+import Contact from "./sections/Contact";
 
 const App = () => {
   return <div className="container mx-auto max-w-7xl">
@@ -14,8 +15,7 @@ const App = () => {
     <Projects/>
     <Experiences/>
     <Testimonial/>
-    <section className="min-h-screen"></section>
-    {/* contact */}
+    <Contact/>
     {/* footer */}
   </div>;
 };
