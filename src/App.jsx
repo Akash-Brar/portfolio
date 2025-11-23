f8e76b5e9d08c16d06e7901c785b3aff31af8eb5
Akash-Brar <akashbrar467@gmail.com>
2025-11-23 14:19:10 -0700

    updated tech stack

--- src/App.jsx
+++ src/App.jsx
@@ -20,5 +20,5 @@ const App = () => {
     <Footer/>
   </div>;
 };
-// test
+
 export default App;
--- src/components/Frameworks.jsx
+++ src/components/Frameworks.jsx
@@ -2,7 +2,7 @@ import { OrbitingCircles } from "./ObitingCirlces";
 
 
 export function Frameworks() {
-    const skills = ["auth0", "blazor", "cplusplus", "csharp", "css3", "dotnet", "dotnetcore", "git", "html5", "javascript", "microsoft", "react", "sqlite", "tailwindcss", "vitejs", "wordpress"]
+    const skills = ["cplusplus", "csharp", "css3", "git", "github", "html5", "javascript", "microsoft", "python", "react", "sqlite", "tailwindcss", "threejs"]
   return (
     <div className="relative flex h-[15rem] w-full flex-col items-center justify-center">
       <OrbitingCircles iconSize={40}>
