5d4b2bcfa9d35e542592cf9ad0d9570f60d78608
Akash-Brar <akashbrar467@gmail.com>
2026-07-21 16:51:06 -0600

    Updated socials

--- src/constants/index.js
+++ src/constants/index.js
@@ -231,12 +231,12 @@ export const mySocials = [
   },
   {
     name: "Linkedin",
-    href: "https://www.linkedin.com/in/ali-sanati/",
+    href: "https://www.linkedin.com/in/",
     icon: "assets/socials/linkedIn.svg",
   },
   {
     name: "Instagram",
-    href: "https://www.instagram.com/ali.sanatidev/reels/",
+    href: "https://www.instagram.com/",
     icon: "assets/socials/instagram.svg",
   },
 ];
